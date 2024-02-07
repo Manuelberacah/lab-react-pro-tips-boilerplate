27b1dd9fdc65d69cdedb7e65502dd8135381c980
Manuelberacah <141898668+Manuelberacah@users.noreply.github.com>
2024-02-07 13:01:29 +0530

    Add files via upload

---
+++ src/Component/Contacts.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Contacts =()=>{
+    return (
+     <>
+        <div id="box">
+            <h1>Contact Manuel</h1>
+        </div>
+     </>   
+    )
+}
+
+export default Contacts;
